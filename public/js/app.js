af3e5a42738fe434a9d66bbf9b6317b61595f2bc
Rocco Orefice <rocco.orefice92@gmail.com>
2021-04-07 15:18:55 +0200

    select from lists of breeds

--- public/js/app.js
+++ public/js/app.js
@@ -49867,7 +49867,8 @@ Vue.component('example-component', __webpack_require__(/*! ./components/ExampleC
  */
 
 var app = new Vue({
-  el: '#app'
+  el: "#app",
+  data: {}
 });
 
 /***/ }),
--- resources/views/dogs.blade.php
+++ resources/views/dogs.blade.php
@@ -1,9 +1,30 @@
-<h1>Breeds list</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">
+        <h1>Breeds list</h1>
+        <select v-model="selection">
+            @foreach ($breeds as $key => $value )
+                <option value="{{ $key }}">
+                    {{$key}}
+                </option>
+            @endforeach
+        </select>
+        
+    </div>
+      
+        
+</body>
+</html>
 
-<ul>
-    @foreach ($breeds as $key => $value )
-        <li>
-            {{$key}}
-        </li>
-    @endforeach
-</ul>    
+
+
+
